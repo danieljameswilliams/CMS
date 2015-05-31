5af8ac1ef0106947a21633542d16064e9ba1f2c9
Daniel James Williams <mail@danielwilliams.dk>
2015-05-31 22:10:03 +0200

    If .get() has no parameters we just get current page pathname

--- frontend/public/javascripts/partials/website/service.js
+++ frontend/public/javascripts/partials/website/service.js
@@ -2,24 +2,29 @@ angular.module('website').factory('Website', function( $http, $q ) {
   return {
     /**
      * Use the .get() method to retrieve data for a specific page, with additional website data.
-     * For demo purposes, see "/interface/mocks/frontend.json"
+     * For demo purposes, see "/mocks/frontend.json"
      * @param  {Array}  Allow one or more pages to be retrieved in one request, takes both ID or pathname
-     * @param  {Boolean}
      * @return {Promise}
      */
-    get: function( pageIDs, pageDataOnly ) {
+    get: function( pageIDs ) {
       // TODO: Cache the response for a while via localStorage, so we dont need to fetch the same data,
       // and extend the response "pages" as they are loaded, for better history.back performance.
       // TODO: Figure out what page to get, from querystring or something.
-      return _getWebsite.call( this, pageIDs, pageDataOnly, $http, $q );
+      return _getWebsite.call( this, pageIDs, $http, $q );
     }
   };
 });
 
-function _getWebsite( pageIDs, pageDataOnly, $http, $q ) {
-  // TODO: Make the request take a PagesArray querystring to pass to server.
-  // TODO: Make the request take a pageDataOnly querystring to pass to server.
-  // TODO: Check the pageIDs parameter, if each is a ID or pathname, and identify accordingly.
+function _getWebsite( pageIDs, $http, $q ) {
+  // TODO: Add to a async task: Collect pageIDs of localStorage pages that are older than some refresh-rate (to be defined) specific for this domain, and refresh the pages.
+
+  // If no parameter was passed in, we just take the current pathname,
+  // to request from remote or localStorage.
+  pageIDs = pageIDs || [ window.location.name ];
+
+
+
+
   var dfrd = $q.defer();
   var response = $http.get('/mocks/frontend.json');
 
