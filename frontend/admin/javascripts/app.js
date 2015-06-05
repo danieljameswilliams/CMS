1015bbabea15a53aae09a0f03275b52cbf6cf5d4
Daniel James Williams <mail@danielwilliams.dk>
2015-06-05 09:03:53 +0200

    Backend Scaffold added

---
+++ frontend/admin/javascripts/app.js
@@ -0,0 +1,16 @@
+/**
+ * Installed Apps,
+ * All modules are put into its own folder with it's own 'controller and service'
+ */
+
+// TODO: Login Service
+// TODO: Page Collections controller (Page structure viewer)
+// TODO: Page Designer (Extensions to Page modules)
+
+
+/**
+ * Main App,
+ * TODO: Check if the modules could be initialized from parent-module instead of from main-app
+ */
+
+var app = angular.module('CMS', []);
